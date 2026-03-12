1d0bec7792565dfb7629ff9d3a08d449a023d44d
KALEL2006 <kalel0319@hotmail.com>
2026-03-12 10:53:18 -0500

    feat. agregar municipio. Closes #43

--- src/main/java/org/fis/grupo1/Municipio.java
+++ src/main/java/org/fis/grupo1/Municipio.java
@@ -1,4 +1,13 @@
 package org.fis.grupo1;
 
 public class Municipio {
+
+    String nombre;
+    Departamento departamento;
+
+    public Municipio(String nombre, Departamento departamento) {
+        this.nombre = nombre;
+        this.departamento = departamento;
+    }
+
 }
