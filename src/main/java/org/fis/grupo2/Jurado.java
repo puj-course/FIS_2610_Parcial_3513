7365ba1802d80a40efc221c085e7a04750d39f63
germandrzmr <germandrzmr@gmail.com>
2026-03-12 10:55:29 -0500

    commit german

---
+++ src/main/java/org/fis/grupo2/Jurado.java
@@ -0,0 +1,7 @@
+package org.fis.grupo2;
+
+public class Jurado {
+    private String cedula;
+    private String nombre;
+    private String cargo;
+}
---
+++ src/main/java/org/fis/grupo2/Mesa.java
@@ -0,0 +1,7 @@
+package org.fis.grupo2;
+
+public class Mesa {
+    private int numeroMesa;
+    private int numeroVotantes;
+    
+}
