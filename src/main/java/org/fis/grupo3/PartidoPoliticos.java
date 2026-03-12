b2f58edbd79c765d2057e488b9b0290fa83dba97
Andres Castrillo <itsbens12@outlook.com>
2026-03-12 10:56:32 -0500

    feat: Agregar atributos y metodos de clase para historia de usuario 6 Closes #14

--- src/main/java/org/fis/grupo3/PartidoPoliticos.java
+++ src/main/java/org/fis/grupo3/PartidoPoliticos.java
@@ -1,5 +1,29 @@
 package org.fis.grupo3;
 
 public class PartidoPoliticos {
-    private String nmobre;
+    private String idPartido;
+    private String nombre;
+    private String sigla;
+    private String representanteLegal;
+
+    private List<Candidatos> candidatosInscritos;
+
+    public PartidoPoliticos(String idPartido, String nombre, String sigla, String representanteLegal, List<Candidato> candidatosInscritos) {
+        this.idPartido = idPartido;
+        this.nombre = nombre;
+        this.sigla = sigla;
+        this.representanteLegal = representanteLegal;
+        this.candidatosInscritos = candidatosInscritos;
+    }
+
+    // Asociar candidato al partido
+    public void agregarCandidato(Candidatos candidato) {
+        candidatosInscritos.add(candidato);
+    }
+
+    // Consultar candidatos del partido
+
+    public List<Candidatos> obtenerCandidatos() {
+        return candidatosInscritos;
+    }
 }
