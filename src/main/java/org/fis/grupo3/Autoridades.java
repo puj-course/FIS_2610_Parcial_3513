f8c967db6600254c4be1d8a5c52ffd8c437644c7
Juanjoyt2 <juanjoarbelaezbz@gmail.com>
2026-03-12 10:49:17 -0500

    feat; clase autoridades con metodos + historial de situaciones anormales. Closes (#25)

--- src/main/java/org/fis/grupo3/Autoridades.java
+++ src/main/java/org/fis/grupo3/Autoridades.java
@@ -1,4 +1,61 @@
 package org.fis.grupo3;
+import java.util.ArrayList;
 
 public class Autoridades {
+	private int idAutoridad;
+	private String nombre;
+	private String apellido;
+	private String cargo;
+	private int numeroVotos;
+	private boolean electo;
 }
+public Autoridades(){
+}
+public int getIdAutoridad(){
+	return idAutoridad;
+}
+public String getNombre(){
+	return nombre;
+}
+
+public String getApellido(){
+	return apellido;
+}
+public String getCargo(){
+	return cargo;
+}
+public int getNumeroVotos(){
+	return numeroVotos;
+}
+public boolean isElecto(){
+	return electo;
+}
+public boid setIdAutoridad(int idAutoridad){
+	this.idAutoridad = idAutoridad;
+}
+public void setNombre(String nombre){
+	this.nombre=nombre;
+}
+public void setApellido(String apelliudo){
+	this.apellido = apellido;
+}
+public void setCargo (String cargo){
+	this.cargo= cargo;
+}
+public void setNumeroVotos (int numeroVotos){
+	this.numeroVotos = numeroVotos;
+}
+public void setElecto (boolean electo){
+	this.electo = electo;
+}
+
+private ArrayList <String> historialSituaciones = new ArrayList <>();
+
+public void registrarSituacionAnormal (String situacion) {
+	historialSituaciones.add(situacion);
+}
+public ArrayList <String> getHistorialSituaciones(){
+	return historialSituaciones;
+}
+}
+
