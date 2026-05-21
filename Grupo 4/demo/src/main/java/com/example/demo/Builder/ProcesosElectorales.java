53f2a253bbf78403ced60e49a6ba53a231c6833c
Alejandro Gonzalez Ochoa <alejandro.gonzalez.o1055@gmail.com>
2026-05-21 10:46:54 -0500

    feat: Patron builder y singleton completos. #102

--- Grupo 4/demo/src/main/java/com/example/demo/Builder/ProcesosElectorales.java
+++ Grupo 4/demo/src/main/java/com/example/demo/Builder/ProcesosElectorales.java
@@ -2,4 +2,6 @@ package com.example.demo.Builder;
 
 public interface ProcesosElectorales {
     void reset();
+    void anadirTipoDeEleccion(String tipoEleccion);
+    ProcesoElectoral getResultado();
 }
