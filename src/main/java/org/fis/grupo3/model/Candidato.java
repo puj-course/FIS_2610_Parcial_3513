0198ad7d9927b290b692ecbc0177128d77024029
echaustre12 <124590551+echaustre12@users.noreply.github.com>
2026-05-21 10:43:59 -0500

    feat: implementar strategy en candidato model. Refs #135

--- src/main/java/org/fis/grupo3/model/Candidato.java
+++ src/main/java/org/fis/grupo3/model/Candidato.java
@@ -3,11 +3,15 @@ package org.fis.grupo3.model;
 public class Candidato {
     private String cedula;
     private String numeroTarjeton;
+    private CandidatoStrategy strategy;
 
     public Candidato(String cedula, String numeroTarjeton) {
         this.cedula = cedula;
         this.numeroTarjeton = numeroTarjeton;
+        this.strategy = new ValidacionCompletaStrategy();
     }
+    public void setStrategy(CandidatoStrategy strategy) { this.strategy = strategy;}
+    public boolean processar() { return strategy.ejecutar(this); }
     public String getCedula (){
         return cedula;
     }
