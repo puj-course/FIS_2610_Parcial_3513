c2bf33e727b3dcbb9bd1d4ea85d865a2c7a68fd4
KALEL2006 <kalel0319@hotmail.com>
2026-05-21 10:25:15 -0500

    feat: Implementacion del patron fachada. Closes #117

---
+++ src/main/java/org/fis/grupo1/parcial2/facade/Municipio.java
@@ -0,0 +1,14 @@
+package org.fis.grupo1.parcial2.facade;
+
+public class Municipio {
+
+    String nombre;
+    String departamento;
+
+    public Municipio(String nombre, String departamento) {
+        this.nombre = nombre;
+        this.departamento = departamento;
+    }
+
+}
+
