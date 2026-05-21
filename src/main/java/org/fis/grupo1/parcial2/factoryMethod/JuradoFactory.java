02112c882c504c994ea81f254646929713128293
Richard Castillo <richard.castillo.1236@gmail.com>
2026-05-21 10:54:07 -0500

    Correcion factory jurado #108

--- src/main/java/org/fis/grupo1/parcial2/factoryMethod/JuradoFactory.java
+++ src/main/java/org/fis/grupo1/parcial2/factoryMethod/JuradoFactory.java
@@ -3,6 +3,9 @@ package org.fis.grupo1.parcial2.factoryMethod;
 public class JuradoFactory implements Factory{
     @Override
     public Persona create(String cedula, int mesa) {
+        if (mesa*-1 < 0){
+            return null;
+        }
         return new JuradoVotacion(cedula, mesa);
     }
 }
